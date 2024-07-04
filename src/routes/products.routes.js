185cddf5d4ea35c6ceb2351069b9bc220ed5d709
juniorpinhodev <juniorpinhodev@gmail.com>
2024-07-04 17:35:15 -0300

    feat: 🎸 criando rota para gerar produtos com detalhes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct, getAllProducts } = require('../controllers/ProductController');
+const { createProduct, getAllProducts, getProductWithDetails } = require('../controllers/ProductController');
 
 router.post('/products', createProduct); //esta rota cria um produto
 
 router.get('/products', getAllProducts); //esta rota lista todos os produtos
+
+router.get('/products/:id', getProductWithDetails); //esta rota gera produtos com detalhes
 
 module.exports = router;
