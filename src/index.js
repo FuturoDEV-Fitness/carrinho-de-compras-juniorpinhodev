a43820ac0026156ed9de4349444c9939a21d86f1
juniorpinhodev <juniorpinhodev@gmail.com>
2024-07-04 18:40:58 -0300

    feat: 🎸 incluindo rotas do orders.routers no index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const clientRoutes = require('./routes/clients.routes');
 const productRoutes = require('./routes/products.routes');
+const orderRoutes = require('./routes/orders.routes');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/api', clientRoutes);
 app.use('/api', productRoutes);
+app.use('/api', orderRoutes);
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
