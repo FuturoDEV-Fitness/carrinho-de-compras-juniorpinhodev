1c35c4df2b7248ac45e9f1fdb97f089b67d64beb
juniorpinhodev <juniorpinhodev@gmail.com>
2024-07-04 17:23:43 -0300

    feat: 🎸 definição da constante getAllProducts

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct } = require('../controllers/ProductController');
+const { createProduct, getAllProducts } = require('../controllers/ProductController');
 
 router.post('/products', createProduct); //esta rota cria um produto
 
