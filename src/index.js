79f816565a58a07927841ddc305039e6f0635aa0
juniorpinhodev <juniorpinhodev@gmail.com>
2024-07-04 16:10:18 -0300

    feat: 🎸 conectando rotas e controle de produtos ao index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const clientRoutes = require('./routes/clients.routes');
+const productRoutes = require('./routes/products.routes');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/api', clientRoutes);
+app.use('/api', productRoutes);
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
